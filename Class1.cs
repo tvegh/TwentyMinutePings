d0263760a807f3ea493f51640ddb39b0373ea258
Tyler Vegh <tyler@vegh.tv>
2019-06-01 17:24:18 -0400

    First version

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+
+
+namespace TwentyMinutePing
+{
+	[BepInDependency("com.bepis.r2api")]
+	[BepInPlugin("com.TeaBoneJones.TwentyMinutePing", "20 Minute Ping", "1.0.0")]
+	public class TwentyMinutePing:BaseUnityPlugin
+	{
+        private static ConfigWrapper<int> minutes;
+        public void Awake()
+        {
+            minutes = Config.Wrap<int>("Ping Indicator", "Minutes", "Set how long you want the ping to be enabled for, in minutes. Default = 20", 20);
+            float seconds = minutes.Value * 60;
+            On.RoR2.UI.PingIndicator.RebuildPing += (orig, self) =>
+            {
+                orig(self);
+                self.SetFieldValue<float>("fixedTimer", seconds);
+            };
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Twenty Minute Pings
+
+Pings stay up for 20 minutes (configurable).
+
+## Config
+
+Change how long the pings stay up for in the config file.
+
+## Known Issues
+
+None so far. But if you find any, create a new issue on the github:
+https://github.com/tvegh/TwentyMinutePings/issues
+
+## Changelog
+
+- 1.0.0: First version. I'm sure there will be no others, because my code is always flawless.
